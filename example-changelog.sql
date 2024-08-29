af0ce0c39390ac58be969b98fb5d452c8c765c11
JMontano99 <alberto.montano@axity.com>
2024-08-29 08:58:42 -0600

    Se crea proyecto-base-datos.sql y en propierties este se asigna como nuevo changelogfile y se generan los changelog .json,.yaml y xml

--- example-changelog.sql
+++ example-changelog.sql
@@ -11,15 +11,33 @@ create table person (
 )
 --rollback DROP TABLE person;
 
---changeset AlbertoMontano:2 labels:example-label context:example-context
---comment: example comment
-create table company (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
+--changeset JoseAlbertoMM
+--comment: Creacion de la tabla Cursos
+CREATE TABLE IF NOT EXISTS Cursos (
+    ID_Curso INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    ID_Instructor INT,
+    Descripcion VARCHAR(50),
+    Estado VARCHAR(50)
+);
+
+--changeset JoseAlbertoMM
+--comment: Creacion de la tabla Materiales
+CREATE TABLE IF NOT EXISTS Materiales (
+    ID_Material INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Curso INT,
+    Nombre VARCHAR(50) NOT NULL,
+    Descripcion VARCHAR(50),
+    FPublicacion DATE,
+    URL VARCHAR(250)
+);
+
+--changeset JoseAlbertoMM
+--comment: Creacion de las llaves foraneas ID_Instructor en tabla cursos y ID_Curso en tabla metariales
+ALTER TABLE Materiales
+ADD CONSTRAINT FK_Materiales_Cursos
+FOREIGN KEY (ID_Curso) REFERENCES cursos(ID_Curso);
+
 --rollback DROP TABLE company;
 
 --changeset other.dev:3 labels:example-label context:example-context
